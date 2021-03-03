1feeda557813de2500b92b20e49acc8bcb2db281
Derek <derekr@me.com>
2021-03-02 20:50:28 -0800

    Lol

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './routes/**/*.{js,ts,jsx,tsx',
+    './routes/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
